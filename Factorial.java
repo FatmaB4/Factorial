9ffa0b600a1de1a8ff28c74ecce33528eda18dcc
studentFatmaFat <78883837+FatmaB4@users.noreply.github.com>
2021-10-31 03:46:14 +0000

    Initial commit

---
+++ Factorial.java
@@ -0,0 +1,15 @@
+public class Factorial
+{
+	public static long getFactorial( int num )
+	{
+		long factorial=1;
+
+    for(int i=num;i>0&&i<=num;i--){
+      factorial=factorial*i;
+    }
+		return factorial;
+	}
+  public static int Number(int num){
+    return num;
+  }
+}
---
+++ Main.java
@@ -0,0 +1,15 @@
+public class Main
+{
+	public static void main ( String[] args )
+	{ 
+    
+     System.out.println("factorial of "+Factorial.Number(5)+" is "+Factorial.getFactorial(5));
+     System.out.println("factorial of "+Factorial.Number(4)+" is "+Factorial.getFactorial(4));
+     System.out.println("factorial of "+Factorial.Number(8)+" is "+Factorial.getFactorial(8));
+     System.out.println("factorial of "+Factorial.Number(15)+" is "+Factorial.getFactorial(15));
+     System.out.println("factorial of "+Factorial.Number(6)+" is "+Factorial.getFactorial(6));
+     System.out.println("factorial of "+Factorial.Number(9)+" is "+Factorial.getFactorial(9));
+     System.out.println("factorial of "+Factorial.Number(3)+" is "+Factorial.getFactorial(3));
+    
+	}
+}
